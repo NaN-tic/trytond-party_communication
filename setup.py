e0022ad0f087a4790572ebad5a7e1e8ce629514e
resteve <resteve@zikzakmedia.com>
2012-07-19 15:51:19 +0200

    Download URL setup.py

--- setup.py
+++ setup.py
@@ -26,8 +26,7 @@ setup(name='trytond_party_communication',
     author=info.get('author', 'Zikzakmedia'),
     author_email=info.get('email', 'zikzak@zikzakmedia.com'),
     url=info.get('website', 'http://www.zikzakmedia.com'),
-    download_url="http://downloads.tryton.org/" + \
-            info.get('version', '0.0.1').rsplit('.', 1)[0] + '/',
+    download_url="https://bitbucket.org/zikzakmedia/trytond-party_communication",
     package_dir={'trytond.modules.party_communication': '.'},
     packages=[
         'trytond.modules.party_communication',
