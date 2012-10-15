10df239ba6e91082f9ce4564c7c0e1d0920428d3
resteve <resteve@zikzakmedia.com>
2012-10-15 11:47:26 +0200

    Active Record

--- __init__.py
+++ __init__.py
@@ -2,4 +2,11 @@
 #The COPYRIGHT file at the top level of this repository contains 
 #the full copyright notices and license terms.
 
+from trytond.pool import Pool
 from .address import *
+
+
+def register():
+    Pool.register(
+        Address,
+        module='party_communication', type_='model')
--- address.py
+++ address.py
@@ -2,19 +2,18 @@
 #The COPYRIGHT file at the top level of this repository contains 
 #the full copyright notices and license terms.
 
-from trytond.model import ModelView, ModelSQL, fields
-from trytond.tools import safe_eval, datetime_strftime
-from trytond.transaction import Transaction
-from trytond.pool import Pool
+from trytond.model import fields
+from trytond.pool import PoolMeta
 
-class Address(ModelSQL, ModelView):
+__all__ = ['Address']
+__metaclass__ = PoolMeta
+
+class Address:
     "Address"
-    _name = 'party.address'
-    _description = __doc__
+    __name__ = 'party.address'
     phone = fields.Char('Phone')
     mobile = fields.Char('Mobile')
     fax = fields.Char('Fax')
     email = fields.Char('E-Mail')
     skype = fields.Char('Skype')
 
-Address()
