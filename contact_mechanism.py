63c06d66febc1571dddc335ec4aaac176ed86072
resteve <resteve@zikzakmedia.com>
2012-12-10 12:46:03 +0100

    Contact Mechanism default Party

--- contact_mechanism.py
+++ contact_mechanism.py
@@ -2,7 +2,7 @@
 #The COPYRIGHT file at the top level of this repository contains 
 #the full copyright notices and license terms.
 from trytond.model import fields
-from trytond.pool import PoolMeta
+from trytond.pool import Pool, PoolMeta
 from trytond.pyson import Eval
 from trytond.transaction import Transaction
 
@@ -20,3 +20,10 @@ class ContactMechanism:
     address = fields.Many2One('party.address', 'Address',
         domain=[('party', '=', Eval('party'))],
         ondelete='CASCADE', states=STATES, select=True, depends=['active', 'party'])
+
+    @classmethod
+    def create(cls, values):
+        if values.get('address'):
+            address = Pool().get('party.address')(values.get('address'))
+            values['party'] = address.party
+        return super(ContactMechanism, cls).create(values)
