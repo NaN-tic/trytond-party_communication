684f88119a489f79e8b99073c465d5be394876bc
resteve <resteve@zikzakmedia.com>
2013-04-26 13:40:54 +0200

    pyflakes fixed

--- address.py
+++ address.py
@@ -3,8 +3,6 @@
 #the full copyright notices and license terms.
 from trytond.model import fields
 from trytond.pool import PoolMeta
-from trytond.pyson import Eval
-from trytond.transaction import Transaction
 
 __all__ = ['Address']
 __metaclass__ = PoolMeta
