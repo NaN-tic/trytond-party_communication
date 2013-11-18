2333e3011bc7a533e593b4f00afa6ec125f0439f
resteve <resteve@zikzakmedia.com>
2013-11-18 16:04:17 +0100

    Revert renamed phone, mobile, fax, email and skype

--- address.py
+++ address.py
@@ -13,15 +13,15 @@ class Address:
     __name__ = 'party.address'
     contact_mechanism = fields.One2Many('party.contact_mechanism', 'address',
         'Contact Mechanism', readonly=True)
-    address_phone = fields.Function(fields.Char('Phone'),
+    phone = fields.Function(fields.Char('Phone'),
         'get_address_contact_mechanism')
-    address_mobile = fields.Function(fields.Char('Mobile'),
+    mobile = fields.Function(fields.Char('Mobile'),
         'get_address_contact_mechanism')
-    address_fax = fields.Function(fields.Char('Fax'),
+    fax = fields.Function(fields.Char('Fax'),
         'get_address_contact_mechanism')
-    address_email = fields.Function(fields.Char('E-Mail'),
+    email = fields.Function(fields.Char('E-Mail'),
         'get_address_contact_mechanism')
-    address_skype = fields.Function(fields.Char('Skype'),
+    skype = fields.Function(fields.Char('Skype'),
         'get_address_contact_mechanism')
 
     def get_address_contact_mechanism(self, name):
