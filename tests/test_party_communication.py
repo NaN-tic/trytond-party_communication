b5f1002ebcde2780604cb8baab8955be20e69216
resteve <resteve@zikzakmedia.com>
2015-11-29 22:34:32 +0100

    Tests

--- tests/test_party_communication.py
+++ tests/test_party_communication.py
@@ -1,27 +1,14 @@
-#!/usr/bin/env python
-#This file is part party_communication module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains 
-#the full copyright notices and license terms.
+# This file is part of the party_communication module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
-import doctest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
-from trytond.tests.test_tryton import doctest_setup, doctest_teardown
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class PartycommunicationTestCase(unittest.TestCase):
-    '''Test Party Communication module'''
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('party_communication')
-
-    def test0005views(self):
-        '''Test views'''
-        test_view('party_communication')
-
-    def test0006depends(self):
-        '''Test depends'''
-        test_depends()
+class PartycommunicationTestCase(ModuleTestCase):
+    'Test Party Communication module'
+    module = 'party_communication'
 
 
 def suite():
